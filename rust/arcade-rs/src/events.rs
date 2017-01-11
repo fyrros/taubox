16a183f30eec5baf4ecbf5da61222b45b5b388bd
Alex Rulkov <fyrros@gmail.com>
2017-01-11 20:01:12 +0300

    new games

---
+++ rust/arcade-rs/src/events.rs
@@ -0,0 +1,45 @@
+// src/events.rs
+
+macro_rules! struct_events {
+    ( /* PATTERN */ ) => {
+        use sdl2::EventPump;
+
+
+        pub struct ImmediateEvents;
+
+        impl ImmediateEvents {
+            pub fn new() -> ImmediateEvents {
+                ImmediateEvents
+            }
+        }
+
+
+        pub struct Events {
+            pump: EventPump,
+            pub now: ImmediateEvents,
+        }
+
+        impl Events {
+            pub fn new(pump: EventPump) -> Events {
+                Events {
+                    pump: pump,
+                    now: ImmediateEvents::new()
+                }
+            }
+
+            pub fn pump(&mut self) {
+                self.now = ImmediateEvents::new();
+
+                for event in self.pump.poll_iter() {
+                    use sdl2::event::Event::*;
+                    use sdl2::keyboard::Keycode::*;
+
+                    match event {
+                        _ => {}
+                    }
+                }
+            }
+        }
+    }
+}
+
